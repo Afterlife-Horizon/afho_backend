490ee5414e603d5ef0c573ee3779e3ac821d4611
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-30 18:04:15 +0100

    s

--- botFiles/api/brasil.js
+++ botFiles/api/brasil.js
@@ -8,10 +8,10 @@ module.exports = function (client) {
         router.get("/", async (req, res) => {
             try {
                 const {moverId, movedId} = req.body;
-                
+
                 const logChannel = await client.channels.cache.get(client.config.baseChannelId);
                 const brasilChannelId = client.config.brasilChannelId;
-            
+
                 const guild = await client.guilds.cache.find(g => g.name === "Afterlife Horizon");
                 const mover = await guild.members.fetch(moverId);
                 const member = await guild.members.fetch(movedId);
@@ -19,12 +19,12 @@ module.exports = function (client) {
 
                 if (!voiceChannel) return res.status(406).json({error: 'You are not in a channel!'});
                 if (!member) return res.status(406).json({error: 'Member is not in a channel!'});
-            
+
                 const filePath = path.resolve(process.env.WORKPATH, `config/movecounts.json`);
                 const data = await fsPromises.readFile(filePath);
                 const moveCounts = JSON.parse(data);
                 let moveCount = moveCounts.filter(m => m.id === member.user.id)[0]?.counter;
-            
+
                 if (!moveCount) {
                     fs.writeFile(filePath, JSON.stringify([...moveCounts, { id: member.user.id.toString(), username: member.user.username, counter: 1 }]), 'utf8', (err) => {
                         if (err) {
