86fd35f1037f0189959cdf6dde394fed424a4b50
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-30 22:16:21 +0100

    added level command

---
+++ botFiles/commands/user/level.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const path = require('path');
+const fsPromises = require('fs/promises');
+require('dotenv').config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('command')
+        .setDescription('get member\'s level!')
+        .addStringOption(option =>
+            option.setName('member')
+                .setDescription('member you want to move!')
+                .setRequired(true)),
+    async execute(interaction) {
+        try {
+            const member = interaction.options.getString('member');
+            const filePath = path.resolve(process.env.WORKPATH, `config/levels.json`);
+            const data = await fsPromises.readFile(filePath);
+            const levels = JSON.parse(data);
+            let lvl = levels.filter(m => m.id === member.user.id)[0]?.lvl;
+            let xp = levels.filter(m => m.id === member.user.id)[0]?.xp;
+
+            if (!lvl || !xp) return interaction.reply({ content: `${member} has not sent any messages yet!` });
+
+            await interaction.reply({ content: `${member}'s level is: ${lvl} with ${xp} messages sent!` });
+        }
+        catch (err) {
+            console.error(err);
+            await interaction.reply({ content: `❌ An error occured!` });
+        }
+    },
+};
+
