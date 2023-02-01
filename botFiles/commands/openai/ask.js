a8adf51c27f192a2c47cdaa9c0ef1d875375fb66
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-02-01 17:10:39 +0100

    changed max tokens

--- botFiles/commands/openai/ask.js
+++ botFiles/commands/openai/ask.js
@@ -18,6 +18,7 @@ module.exports = {
             model: "text-davinci-003",
             prompt: interaction.options.getString('question'),
             temperature: 0.6,
+            max_tokens: 2000,
           });
 
           if (completion.data.choices[0].text.length > 2000) {
