1e1238e828c29337ae5ade72aeb2b437ee6c8260
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-02-01 19:02:57 +0100

    added image generation command

---
+++ botFiles/commands/openai/genImage.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { Configuration, OpenAIApi } = require("openai");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('image')
+        .setDescription('generate an image with openai!')
+        .addStringOption(option => option.setName('request').setDescription('the image you want to generate').setRequired(true)
+        .addStringOption(option => option.setName('size').setDescription('the size of the image (1024x1024)').setRequired(false))),
+    async execute(interaction) {
+
+        const configuration = new Configuration({
+            apiKey: "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",
+        });
+        const openai = new OpenAIApi(configuration);
+    
+        interaction.reply({ content: 'Thinking...' });
+
+        const response = await openai.createImage({
+            prompt: interaction.options.getString('request'),
+            n: 1,
+            size: interaction.options.getString('size') || '1024x1024',
+          });
+
+          interaction.editReply({ content: response.data.data[0].url });
+    },
+};
