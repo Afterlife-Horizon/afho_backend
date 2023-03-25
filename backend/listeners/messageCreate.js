66848210a370cad7253eefb4c615f5634becba62
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-03-25 12:44:18 +0100

    fix again

--- backend/listeners/messageCreate.js
+++ backend/listeners/messageCreate.js
@@ -28,7 +28,7 @@ module.exports = function (client) {
                         });
                     }
                     else {
-                        updateDB("afho", "INSERT INTO bot_levels(id, xp) VALUES (?, 1)", [message.author.id], (err) => {
+                        updateDB("afho", "INSERT INTO bot_levels(id, username, xp) VALUES (?, 1)", [message.author.username, message.author.id], (err) => {
                             if (err) {
                                 console.error(err);
                             }
