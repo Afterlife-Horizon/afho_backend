dc8ce9c7437020c329a3df1257088bb1b7e864d4
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-03-26 19:23:48 +0200

    test

--- frontend/src/components/Favs.tsx
+++ frontend/src/components/Favs.tsx
@@ -17,6 +17,9 @@ const Favs: React.FC = () => {
 
 	const [favAdd, setFavAdd] = useState("");
 	const [page, setPage] = useState(1);
+
+	if(!favs.length) return <div></div>;
+
 	let maxPage = favs.length > 6 ? Math.ceil((favs.length - 1) / 5) : -1;
 	if (page > maxPage + 2) setPage(maxPage + 2);
 	else if (
