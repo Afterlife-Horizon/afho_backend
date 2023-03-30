6371cb8b24fb488c46ce8d0e95192356a409ffce
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-03-30 18:55:50 +0200

    test

--- src/botClient/BotClient.ts
+++ src/botClient/BotClient.ts
@@ -257,7 +257,7 @@ export default class BotClient extends Client {
         const stream = ytdl(this.getYTLink(songInfoId), requestOpts).once('error', (err) => console.error(err.message, '\n', err.stack))
 
 
-        const newStream = FFmpeg(stream).audioChannels(2).audioBitrate(128).audioFrequency(48000).noVideo().addOptions(encoderArgs).on('error', (err) => console.error(err.message, '\n', err.stack))
+        const newStream = FFmpeg(stream).audioChannels(2).audioBitrate(128).audioFrequency(48000).noVideo().on('error', (err) => console.error(err.message, '\n', err.stack))
 
         const passThrought = new PassThrough();
         newStream.seekInput(seekTime / 1000).writeToStream(passThrought);
