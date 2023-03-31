1368a463f2da044db1a8d8c0c839269e4c2cec8a
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-03-31 21:56:34 +0200

    test

--- frontend/src/components/Favs.tsx
+++ frontend/src/components/Favs.tsx
@@ -16,6 +16,8 @@ const Favs: React.FC = () => {
 	const userId = user?.user_metadata.provider_id || ""
 	const username = user?.user_metadata.full_name || ""
 
+	console.log(user)
+
 	const { data: favs, isLoading: isLoading, isError: isError } = useFavorites(userId)
 
 	if (isLoading) return <Spinner />
