3f6ba78dee8424e50885f19ae5b361e214b19433
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-01 13:22:11 +0200

    test

--- frontend/src/pages/Music.tsx
+++ frontend/src/pages/Music.tsx
@@ -191,7 +191,7 @@ const Music = (props: any) => {
 		<div className={classes}>
 			<MusicContext.Provider value={musicContextValue}>
 				<NowplayingCard />
-				<Queue />
+				{/* <Queue /> */}
 				{/* <Favs /> */}
 				<Filters />
 			</MusicContext.Provider>
