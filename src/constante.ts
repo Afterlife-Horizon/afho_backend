230d9d0c0512d679ec47ff8866c5931a0380c8e2
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-05 19:06:51 +0200

    test

--- src/constante.ts
+++ src/constante.ts
@@ -1,6 +1,6 @@
 import { IReactionRole } from "./types";
 
-export const reactionRoles: IReactionRole[] | undefined = [{
-    "emoji": ":cancer:",
+export const reactionRoles: IReactionRole[] = [{
+    "emoji": "<:kmsfml:1061398041204105377>",
     "roleID": "1093209218410086480"
 }]
