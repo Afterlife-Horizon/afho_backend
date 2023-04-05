8bc0d3985662ddf36dc428f1a3ce98e285c17ef8
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-05 20:08:23 +0200

    change readme

--- src/constante.ts
+++ src/constante.ts
@@ -1,7 +1,7 @@
 import { IReactionRole } from "./types";
 
 export const reactionRoles: IReactionRole[] = [{
-    "description": "star citizen role",
-    "emojiName": "zemmourcat",
-    "roleID": "941717032515301426"
+    description: "star citizen role",
+    emojiName: "zemmourcat",
+    roleID: "941717032515301426"
 }]
