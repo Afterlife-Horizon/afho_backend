d87d732ffb4a3f690bc3d3eab8317aad6baea5b7
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-06 19:58:16 +0200

    test

--- src/botClient/listeners/guildAuditLogEntryCreate.ts
+++ src/botClient/listeners/guildAuditLogEntryCreate.ts
@@ -4,12 +4,11 @@ import BotClient from "../BotClient"
 export default function (client: BotClient) {
 	return client.on(Events.GuildAuditLogEntryCreate, async auditLog => {
 		const { action, executorId, target, targetId } = auditLog
+		console.log(auditLog)
 
 		// Check only for deleted messages.
 		if (action !== AuditLogEvent.MemberMove) return
 
 		// check if the target is a user and waws moved in bresil
-
-		console.log(auditLog)
 	})
 }
