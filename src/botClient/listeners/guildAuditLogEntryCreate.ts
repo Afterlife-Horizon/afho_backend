4a546144dc5b7eedb339096e560082ca3bef317b
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-06 20:15:11 +0200

    test

--- src/botClient/listeners/guildAuditLogEntryCreate.ts
+++ src/botClient/listeners/guildAuditLogEntryCreate.ts
@@ -2,6 +2,6 @@ import BotClient from "../BotClient"
 
 export default function (client: BotClient) {
 	return client.on("guildAuditLogEntryCreate", async auditLog => {
-		console.log(auditLog)
+		console.log("guildAuditLogEntryCreate")
 	})
 }
