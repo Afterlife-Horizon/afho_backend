b3dbf1c8457d565ba36f063630d75e103d0ef177
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-06 20:12:50 +0200

    test

--- src/botClient/listeners/guildAuditLogEntryCreate.ts
+++ src/botClient/listeners/guildAuditLogEntryCreate.ts
@@ -1,16 +1,7 @@
-import { Events, AuditLogEvent } from "discord.js"
 import BotClient from "../BotClient"
-import { Logger } from "../../logger/Logger"
 
 export default function (client: BotClient) {
-	Logger.log("Loading guildAuditLogEntryCreate listener")
-	return client.on(Events.GuildAuditLogEntryCreate, async auditLog => {
+	return client.on("guildAuditLogEntryCreate", async auditLog => {
 		console.log(auditLog)
-		const { action, executorId, target, targetId } = auditLog
-
-		// Check only for deleted messages.
-		if (action !== AuditLogEvent.MemberMove) return
-
-		// check if the target is a user and waws moved in bresil
 	})
 }
--- src/botClient/listeners/voiceStateUpdate.ts
+++ src/botClient/listeners/voiceStateUpdate.ts
@@ -2,39 +2,36 @@ import { getVoiceConnection } from "@discordjs/voice"
 import BotClient from "../BotClient"
 
 export default function (client: BotClient) {
-	return (
-		// ------------ Checking channels voice state updates ------------
-		client.on("voiceStateUpdate", async (oldState, newState) => {
-			if (newState.id == client.user?.id) return
+	return client.on("voiceStateUpdate", async (oldState, newState) => {
+		if (newState.id == client.user?.id) return
 
-			function stateChange(one, two) {
-				return (one === false && two === true) || (one === true && two === false)
-			}
+		function stateChange(one, two) {
+			return (one === false && two === true) || (one === true && two === false)
+		}
 
-			if (
-				stateChange(oldState.streaming, newState.streaming) ||
-				stateChange(oldState.serverDeaf, newState.serverDeaf) ||
-				stateChange(oldState.serverMute, newState.serverMute) ||
-				stateChange(oldState.selfDeaf, newState.selfDeaf) ||
-				stateChange(oldState.selfMute, newState.selfMute) ||
-				stateChange(oldState.selfVideo, newState.selfVideo) ||
-				stateChange(oldState.suppress, newState.suppress)
-			) {
+		if (
+			stateChange(oldState.streaming, newState.streaming) ||
+			stateChange(oldState.serverDeaf, newState.serverDeaf) ||
+			stateChange(oldState.serverMute, newState.serverMute) ||
+			stateChange(oldState.selfDeaf, newState.selfDeaf) ||
+			stateChange(oldState.selfMute, newState.selfMute) ||
+			stateChange(oldState.selfVideo, newState.selfVideo) ||
+			stateChange(oldState.suppress, newState.suppress)
+		) {
+			return
+		}
+
+		// channel joins
+		if (!oldState.channelId && newState.channelId) return
+
+		// channel leaves
+		if ((!newState.channelId && oldState.channelId) || (newState.channelId && oldState.channelId)) {
+			setTimeout(() => {
+				const connection = getVoiceConnection(newState.guild.id)
+				if (oldState.channel ? oldState.channel.members.filter(m => !m.user.bot).size >= 1 : true) return
+				if (connection && connection.joinConfig.channelId == oldState.channelId) connection.destroy()
 				return
-			}
-
-			// channel joins
-			if (!oldState.channelId && newState.channelId) return
-
-			// channel leaves
-			if ((!newState.channelId && oldState.channelId) || (newState.channelId && oldState.channelId)) {
-				setTimeout(() => {
-					const connection = getVoiceConnection(newState.guild.id)
-					if (oldState.channel ? oldState.channel.members.filter(m => !m.user.bot).size >= 1 : true) return
-					if (connection && connection.joinConfig.channelId == oldState.channelId) connection.destroy()
-					return
-				}, 15000)
-			}
-		})
-	)
+			}, 15000)
+		}
+	})
 }
