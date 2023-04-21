17ddd356a1a875446646e494e4f5433946d4cc66
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-21 19:33:18 +0200

    fixed response for playfirst

--- src/api/routes/music/playFirst.ts
+++ src/api/routes/music/playFirst.ts
@@ -20,24 +20,24 @@ export default function (client: BotClient) {
 
 		try {
 			const guild = await client.guilds.fetch(client.config.serverID)
-			if (!guild) return res.status(406).send("Guild not found!")
+			if (!guild) return res.status(406).send({ error: "Guild not found!" })
 			const connectedMembers = guild.members.cache.filter(member => member.voice.channel)
 			const requester = connectedMembers.find(member => member.user.username === user.data.user?.user_metadata.full_name)
 
-			if (!requester) return res.status(406).send("You are not connected to a voice channel!")
+			if (!requester) return res.status(406).send({ error: "You are not connected to a voice channel!" })
 
 			const voiceChannel = guild.channels.cache.find(
 				c => c.type === 2 && c.members.filter(m => m.user.username === user.data.user?.user_metadata.full_name).size > 0
 			)
-			if (client.currentChannel?.id !== voiceChannel?.id) return res.status(406).send("Not the same channel!")
+			if (client.currentChannel?.id !== voiceChannel?.id) return res.status(406).send({ error: "Not the same channel!" })
 
-			if (!client.currentChannel) return res.status(406).send("not connected!")
+			if (!client.currentChannel) return res.status(406).send({ error: "not connected!" })
 			const currentChannel = (await client.channels.fetch(client.currentChannel.id)) as VoiceChannel | TextChannel
 
 			const channel = (await client.channels.fetch(client.config.baseChannelID)) as VoiceChannel | TextChannel
 			const queue = client.queues.get(currentChannel.guild.id)
 
-			if (!queue) return res.status(406).send("No queue found!")
+			if (!queue) return res.status(406).send({ error: "No queue found!" })
 			const track = req.body.songs
 
 			const youtubRegex = /^(https?:\/\/)?(www\.)?(m\.|music\.)?(youtube\.com|youtu\.?be)\/.+$/gi
@@ -62,14 +62,18 @@ export default function (client: BotClient) {
 			} else {
 				song = await YouTube.searchOne(track)
 			}
-			if (!song && !playlist) return channel.send(`❌ **Failed looking up for ${track}!**`)
+			if (!song && !playlist) {
+				res.status(406).send({ error: "Failed looking up for ${track}!" })
+				return channel.send(`❌ **Failed looking up for ${track}!**`)
+			}
 			if (!playlist) {
 				const video = song as Video
 				if (!queue) {
-					res.status(406).send("No queue")
+					res.status(406).send({ error: "No queue" })
 					return channel.send({ content: `❗ No queue!` })
 				}
 				queue.tracks = [queue.tracks[0], client.createSong(video, user.data.user?.user_metadata.full_name), ...queue.tracks.slice(1)]
+				res.status(200).send(`▶️ Queued at 1st: ${video.title} - ${video.durationFormatted}`)
 				return channel.send(`▶️ **Queued at \`1st\`: __${video.title}__** - \`${video.durationFormatted}\``)
 			} else {
 				song = song ? song : playlist.videos[0]
@@ -90,11 +94,15 @@ export default function (client: BotClient) {
 						playlist.videos.length - 1
 					} Songs\` from the Playlist:**\n> __**${playlist.title}**__`
 				)
-				res.status(200).send("OK")
+				res.status(200).send(
+					`👍 **Queued at \`1st\`: __${video.title}__** - \`${video.durationFormatted}\`\n> **Added \`${
+						playlist.videos.length - 1
+					} Songs\` from the Playlist:**\n> __**${playlist.title}**__`
+				)
 			}
 		} catch (err) {
 			if (err instanceof Error) Logger.error(err.message)
-			res.status(500).send("OK")
+			res.status(500).send({ error: "Something went wrong!" })
 		}
 	})
 }
