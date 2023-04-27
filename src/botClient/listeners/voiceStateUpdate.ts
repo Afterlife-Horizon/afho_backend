4da7506d3088e12015efcfea45407aa17ef84e14
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-27 13:35:34 +0200

    stop timer if muted or deafen

--- src/botClient/listeners/voiceStateUpdate.ts
+++ src/botClient/listeners/voiceStateUpdate.ts
@@ -1,30 +1,80 @@
 import { getVoiceConnection } from "@discordjs/voice"
 import BotClient from "../BotClient"
-import { AuditLogEvent } from "discord.js"
+import { AuditLogEvent, VoiceState } from "discord.js"
 import { Logger } from "../../logger/Logger"
 
 export default function (client: BotClient) {
 	return client.on("voiceStateUpdate", async (oldState, newState) => {
 		if (newState.id == client.user?.id) return
 
-		function stateChange(one: boolean | null, two: boolean | null) {
-			return (one === false && two === true) || (one === true && two === false)
+		switch (getStateAction(oldState, newState)) {
+			case "join":
+				handleUpdateTimer(true, oldState, newState)
+				break
+			case "leave":
+				handleUpdateTimer(false, oldState, newState)
+				handleChannelLeave(oldState, newState)
+				break
+			case "server undeafen":
+				handleUpdateTimer(true, oldState, newState)
+				break
+			case "self undeafen":
+				handleUpdateTimer(true, oldState, newState)
+				break
+			case "server unmute":
+				handleUpdateTimer(true, oldState, newState)
+				break
+			case "self unmute":
+				handleUpdateTimer(true, oldState, newState)
+				break
+			case "move":
+				await handleChannelMove(newState)
+				break
+		}
+	})
+
+	function getStateAction(oldState: VoiceState, newState: VoiceState) {
+		if (!newState.channelId && oldState.channelId) {
+			Logger.log(`User ${oldState.member?.user.username} left ${oldState.channel?.name} in ${oldState.guild.name}`)
+			return "leave"
+		}
+		if (newState.channelId && !oldState.channelId) {
+			Logger.log(`User ${newState.member?.user.username} joined ${newState.channel?.name} in ${newState.guild.name}`)
+			return "join"
 		}
 
-		if (
-			stateChange(oldState.streaming, newState.streaming) ||
-			stateChange(oldState.serverDeaf, newState.serverDeaf) ||
-			stateChange(oldState.serverMute, newState.serverMute) ||
-			stateChange(oldState.selfDeaf, newState.selfDeaf) ||
-			stateChange(oldState.selfMute, newState.selfMute) ||
-			stateChange(oldState.selfVideo, newState.selfVideo) ||
-			stateChange(oldState.suppress, newState.suppress)
-		)
-			return
+		if (oldState.serverDeaf && !newState.serverDeaf) {
+			Logger.log(`User ${oldState.member?.user.username} was undeafened in ${oldState.guild.name}`)
+			return "server undeafen"
+		}
 
-		// manage channel joins and leaves to increase time in the channels
-		if (!oldState.channelId && newState.channelId) {
-			// channel joins
+		if (oldState.selfDeaf && !newState.selfDeaf) {
+			Logger.log(`User ${oldState.member?.user.username} was undeafened in ${oldState.guild.name}`)
+			return "self undeafen"
+		}
+
+		if (oldState.serverMute && !newState.serverMute) {
+			Logger.log(`User ${oldState.member?.user.username} was unmuted in ${oldState.guild.name}`)
+			return "server unmute"
+		}
+
+		if (oldState.selfMute && !newState.selfMute) {
+			Logger.log(`User ${oldState.member?.user.username} was unmuted in ${oldState.guild.name}`)
+			return "self unmute"
+		}
+
+		if (oldState.channelId && newState.channelId && oldState.channelId != newState.channelId) {
+			Logger.log(
+				`User ${newState.member?.user.username} moved from ${oldState.channel?.name} to ${newState.channel?.name} in ${newState.guild.name}`
+			)
+			return "move"
+		}
+
+		return "none"
+	}
+
+	function handleUpdateTimer(enter: boolean, oldState: VoiceState, newState: VoiceState) {
+		if (enter) {
 			if (!newState.member?.id) return Logger.log("No member id found")
 			if (!newState.guild.id) return Logger.log("No guild id found")
 			if (!newState.channelId) return Logger.log("No channel id found")
@@ -34,82 +84,75 @@ export default function (client: BotClient) {
 			client.times.set(newState.member.id, new Date())
 		}
 
-		if (!newState.channelId && oldState.channelId) {
+		if (!enter) {
 			if (!oldState.member?.id) return
 			if (oldState.member.id == client.user?.id) return
-			Logger.log(`User ${oldState.member.user.username} left ${oldState.channel?.name} in ${oldState.guild.name}`)
+
 			client.pushTime(oldState.member.id)
 			client.times.delete(oldState.member.id)
 		}
+	}
 
-		if (oldState.channelId && newState.channelId && oldState.channelId != newState.channelId) {
-			// channel moves
-			const fetchedLogs = await newState.guild.fetchAuditLogs({
-				type: AuditLogEvent.MemberMove,
-				limit: 1
+	async function handleChannelMove(newState: VoiceState) {
+		const fetchedLogs = await newState.guild.fetchAuditLogs({
+			type: AuditLogEvent.MemberMove,
+			limit: 1
+		})
+
+		const firstEntry = fetchedLogs.entries.first()
+
+		if (!firstEntry) return
+
+		if (firstEntry?.extra.channel.id == newState.channelId) {
+			if (firstEntry?.executor?.id == client.user?.id) return
+			if (!(newState.channel?.id == client.config.brasilChannelID)) return
+
+			const mover = firstEntry.executor
+			const moved = newState.member
+
+			if (!mover || !moved) return
+
+			await client.prisma.bot_bresil.upsert({
+				where: {
+					id: moved.user.id
+				},
+				update: {
+					bresil_received: {
+						increment: 1
+					}
+				},
+				create: {
+					id: moved.user.id,
+					username: moved.user.username,
+					bresil_received: 1,
+					bresil_sent: 0
+				}
 			})
 
-			const firstEntry = fetchedLogs.entries.first()
-
-			if (firstEntry?.extra.channel.id == newState.channelId) {
-				if (firstEntry?.executor?.id == client.user?.id) return
-				if (!(newState.channel?.id == client.config.brasilChannelID)) return
-
-				const mover = firstEntry.executor
-				const moved = newState.member
-
-				if (!mover || !moved) return
-
-				await client.prisma.bot_bresil.upsert({
-					where: {
-						id: moved.user.id
-					},
-					update: {
-						bresil_received: {
-							increment: 1
-						}
-					},
-					create: {
-						id: moved.user.id,
-						username: moved.user.username,
-						bresil_received: 1,
-						bresil_sent: 0
+			await client.prisma.bot_bresil.upsert({
+				where: {
+					id: mover.id
+				},
+				update: {
+					bresil_sent: {
+						increment: 1
 					}
-				})
-
-				await client.prisma.bot_bresil.upsert({
-					where: {
-						id: mover.id
-					},
-					update: {
-						bresil_sent: {
-							increment: 1
-						}
-					},
-					create: {
-						id: mover.id,
-						username: mover.username,
-						bresil_received: 0,
-						bresil_sent: 1
-					}
-				})
-			}
-
-			Logger.log(
-				`User ${newState.member?.user.username} moved from ${oldState.channel?.name} to ${newState.channel?.name} in ${newState.guild.name}`
-			)
-
-			return
+				},
+				create: {
+					id: mover.id,
+					username: mover.username,
+					bresil_received: 0,
+					bresil_sent: 1
+				}
+			})
 		}
+	}
 
-		// channel leaves
-		if ((!newState.channelId && oldState.channelId) || (newState.channelId && oldState.channelId)) {
-			setTimeout(() => {
-				const connection = getVoiceConnection(newState.guild.id)
-				if (oldState.channel ? oldState.channel.members.filter(m => !m.user.bot).size >= 1 : true) return
-				if (connection && connection.joinConfig.channelId == oldState.channelId) connection.destroy()
-				return
-			}, 15000)
-		}
-	})
+	function handleChannelLeave(oldState: VoiceState, newState: VoiceState) {
+		setTimeout(() => {
+			const connection = getVoiceConnection(newState.guild.id)
+			if (oldState.channel ? oldState.channel.members.filter(m => !m.user.bot).size >= 1 : true) return
+			if (connection && connection.joinConfig.channelId == oldState.channelId) connection.disconnect()
+		}, 15000)
+	}
 }
